fe061acc03ae0e83a479deef2407ba6cba2b2d8b
janerik <janerikb@gmail.com>
2015-02-16 23:15:57 +0100

    Add displayName to component Layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,19 +2,22 @@ const React = require('react');
 
 module.exports = React.createClass({
 
-  render() {
-    return (
-      <html>
-        <head>
-          <script src="/bundle.js"></script>
-        </head>
-        <h1>React / browserify boilerplate</h1>
-      </html>
-    );
-  },
+  displayName: 'Layout',
 
   componentDidMount() {
     console.log('Component did mount');
+  },
+
+  render() {
+
+    return (
+        <html>
+          <head>
+            <script src="/bundle.js"></script>
+          </head>
+          <h1>React / browserify boilerplate</h1>
+        </html>
+    );
   }
 
 });
