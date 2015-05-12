87ec29be3f40a009908517855709d39a3f923e3e
janerik <janerikb@gmail.com>
2015-05-12 20:24:15 +0200

    Use path.join to concat __dirname and folder 'static'

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
+var path = require("path");
 module.exports = {
   devtool: "source-map",
   entry: "./browser/main",
   output: {
-    path: __dirname + "/static",
+    path: path.join(__dirname, "/static"),
     filename: "bundle.js"
   },
   module: {
     loaders: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'}
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader"}
     ]
   }
 };
