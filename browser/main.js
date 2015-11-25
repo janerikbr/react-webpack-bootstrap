fca52a43d13c17e8ad2bda54761b60b3185a2446
janerikbr <janerikb@gmail.com>
2015-11-25 20:59:35 +0100

    Rewrite import of Content.jsx to es6 module format

--- browser/main.js
+++ browser/main.js
@@ -1,15 +1,15 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Content from '../components/Content.jsx';
 import domready from 'domready';
 const knownComponents = {
-  Content: require('../components/Content.jsx'),
+  Content,
 };
 
 domready(() => {
   const serverSideData = JSON.parse(document.getElementById('__react_server_side_data').innerHTML);
   const RenderedComponent = knownComponents[serverSideData.component];
   const initialProps = serverSideData.props;
-  const target = document.getElementById('content');
-  ReactDOM.render(<RenderedComponent {...initialProps}/>, target);
+  ReactDOM.render(<RenderedComponent {...initialProps}/>, document.getElementById('content'));
 });
